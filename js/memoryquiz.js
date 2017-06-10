cbb0bf5b4e56f2ec797603e36eb47e74f7d9b0f5
Brian Hudson <brianhudson@Brians-MacBook-Pro.local>
2017-06-09 22:51:13 -0400

    Combined limitations and memory items quiz pages into one and made slight change to js prompt to reflect that.

--- js/memoryquiz.js
+++ js/memoryquiz.js
@@ -5,7 +5,7 @@ const takequiz = document.getElementById('takequiz');
 //Assign a mouseover event to the Limitations headline that triggers a dialog box with link to the limitations quizk (the "OK" button)
 
 takequiz.addEventListener('mouseover', () => {
-	var x = confirm("Would you like to test your Memory Items knowledge? Click 'OK' for yes, 'Cancel' for no.");
+	var x = confirm("Would you like to test your Memory Items and Limitations knowledge? Click 'OK' for yes, 'Cancel' for no.");
 	if (x) {
 		location.assign('../html/memoryquiz.html');
 	}
