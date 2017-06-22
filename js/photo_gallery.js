83a01e7572b61c5befe3838fa2dfcd99e9b8d671
Brian Hudson <brianhudson@Brians-MacBook-Pro.local>
2017-06-21 22:58:36 -0400

    Added a third button to photo gallery, as well as added functionality to each button so that pertinent tags were associated with each respective  button's click event.

--- js/photo_gallery.js
+++ js/photo_gallery.js
@@ -2,12 +2,12 @@
 
 $(document).ready(function () { //begin ready
 
+	/*~~~~~~ FUNCTION RUN IF FIRST BUTTON ('A320') IS CLICKED ~~~~~~*/
+
 	//highlight the buttons when they're clicked and/or moused over
-	$('button').click(function () {
+	$('#A320').click(function () {
 		$('button').removeClass("selected");
 		$(this).addClass("selected");
-
-	
 
 
 		//Variable holds url with JSON query string (derived from the Flickr API)
@@ -16,8 +16,48 @@ $(document).ready(function () { //begin ready
 		//var planeType = $(this).text(); This doesn't do anything but I'm not deleting it yet
 		
 		//this stores the data to put in the Ajax request
-		var flickrOptions = {
+		var flickr320 = {
 			tags: "A320", 
+			format: "json"
+		};
+
+
+		//This is the callback function which uses the data to build up the html displaying the pics
+		function displayPhotos(data) {
+			var photoHTML = '<ul id="photos">';
+			$.each(data.items, function (i, photo) {
+				photoHTML += '<li class="grid-25">';
+				photoHTML += '<a href="' + photo.link + '" class="image">';
+				photoHTML += '<img src="' + photo.media.m + '"</a></li>';
+			});
+			photoHTML += '</ul>';
+			$('#photos').html(photoHTML);
+		}
+
+		//This is the AJAX send request
+		$.getJSON(flickrAPI, flickr320, displayPhotos);
+
+	});
+
+	
+/* ~~~~~~~ SAME FUNCTION RUN IF THE SECOND BUTTON ('A321') IS CLICKED ~~~~~~*/
+
+		//highlight the buttons when they're clicked and/or moused over
+	$('#A321').click(function () {
+		$('button').removeClass("selected");
+		$(this).addClass("selected");
+
+
+
+
+		//Variable holds url with JSON query string (derived from the Flickr API)
+		var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"; 
+		
+		//var planeType = $(this).text(); This doesn't do anything but I'm not deleting it yet
+		
+		//this stores the data to put in the Ajax request
+		var flickr321 = {
+			tags: "A321", 
 			format: "json"
 		};
 
@@ -34,11 +74,48 @@ $(document).ready(function () { //begin ready
 		}
 
 		//This is the AJAX send request
-		$.getJSON(flickrAPI, flickrOptions, displayPhotos);
+		$.getJSON(flickrAPI, flickr321, displayPhotos);
 
 	});
 
-	
+	/* ~~~~~~~ SAME FUNCTION RUN IF THE THIRD BUTTON ('A330') IS CLICKED ~~~~~~*/
+
+		//highlight the buttons when they're clicked and/or moused over
+	$('#A330').click(function () {
+		$('button').removeClass("selected");
+		$(this).addClass("selected");
+
+
+
+
+		//Variable holds url with JSON query string (derived from the Flickr API)
+		var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"; 
+		
+		//var planeType = $(this).text(); This doesn't do anything but I'm not deleting it yet
+		
+		//this stores the data to put in the Ajax request
+		var flickr330 = {
+			tags: "A330", 
+			format: "json"
+		};
+
+		//This is the callback function which uses the data to build up the html displaying the pics
+		function displayPhotos(data) {
+			var photoHTML = '<ul id="photos">';
+			$.each(data.items, function (i, photo) {
+				photoHTML += '<li class="grid-25">';
+				photoHTML += '<a href="' + photo.link + '" class="image">';
+				photoHTML += '<img src="' + photo.media.m + '"</a></li>';
+			});
+			photoHTML += '</ul>';
+			$('#photos').html(photoHTML);
+		}
+
+		//This is the AJAX send request
+		$.getJSON(flickrAPI, flickr330, displayPhotos);
+
+	});
+
 
 
 
