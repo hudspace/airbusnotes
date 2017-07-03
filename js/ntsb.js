1ff34c6d384efe1af2779fce6eb6ae54b9721813
Brian Hudson <hudson_ops@yahoo.com>
2017-07-03 10:53:50 -0400

    Added 'Under construction' images to Systems and Best Practices pages. Removed 'Go' submit button from Accident Reports dropdown menu and added onchange event handler to make the links in the menu work without the button. Added transform:scale() property to make the featured image in the header smaller on tiny screens.

---
+++ js/ntsb.js
@@ -0,0 +1,15 @@
+
+
+
+
+/*function newPage() {
+
+	//Select an option from the dropdown menu and put it in the 'ntsb' variable
+	const ntsb = document.getElementById('mobile_menu');
+
+	//get the value of the selected option, in this case a url (now in the ntsb variable), and assign it to the 'report' variable
+	const report = ntsb.value;
+
+	//Load the page associated with the newly assigned url value
+	window.location.assign(report);
+}
